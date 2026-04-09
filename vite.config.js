f3ec3e39b618b2b915ba8b6cd48d9d25f568832c
yangweikang <6537498+mo_mo_yang@user.noreply.gitee.com>
2026-04-09 15:17:32 +0800

    解决页面空白

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import { resolve } from 'path';
 import { tmpdir } from 'os';
 import { devLogger } from '@meituan-nocode/vite-plugin-dev-logger';
 import {
-  devHtmlTransformer,
-  prodHtmlTransformer,
+    devHtmlTransformer,
+    prodHtmlTransformer,
 } from '@meituan-nocode/vite-plugin-nocode-html-transformer';
 import react from '@vitejs/plugin-react';
 
@@ -14,59 +14,59 @@ const PUBLIC_PATH = process.env.PUBLIC_PATH || '';
 
 const isProdEnv = process.env.NODE_ENV === 'production';
 const publicPath = (isProdEnv && CHAT_VARIABLE)
-  ? PUBLIC_PATH + '/' + CHAT_VARIABLE
-  : PUBLIC_PATH + '/';
+    ? PUBLIC_PATH + '/' + CHAT_VARIABLE
+    : PUBLIC_PATH + '/';
 const outDir = (isProdEnv && CHAT_VARIABLE) ? 'build/' + CHAT_VARIABLE : 'build';
 
 async function loadPlugins() {
-  const plugins = isProdEnv
-  ? CHAT_VARIABLE
-    ? [react(), prodHtmlTransformer(CHAT_VARIABLE)]
-    : [react()]
-  : [
-      devLogger({
-        dirname: resolve(tmpdir(), '.nocode-dev-logs'),
-        maxFiles: '3d',
-      }),
-      react(),
-      devHtmlTransformer(CHAT_VARIABLE),
-    ];
+    const plugins = isProdEnv
+        ? CHAT_VARIABLE
+            ? [react(), prodHtmlTransformer(CHAT_VARIABLE)]
+            : [react()]
+        : [
+            devLogger({
+                dirname: resolve(tmpdir(), '.nocode-dev-logs'),
+                maxFiles: '3d',
+            }),
+            react(),
+            devHtmlTransformer(CHAT_VARIABLE),
+        ];
 
-  if (process.env.NOCODE_COMPILER_PATH) {
-    const { componentCompiler } = await import(process.env.NOCODE_COMPILER_PATH);
-    plugins.push(componentCompiler());
-  }
-  return plugins;
+    if (process.env.NOCODE_COMPILER_PATH) {
+        const { componentCompiler } = await import(process.env.NOCODE_COMPILER_PATH);
+        plugins.push(componentCompiler());
+    }
+    return plugins;
 }
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => {
-  const plugins = await loadPlugins();
-  
-  return {
-    server: {
-      host: '::',
-      port: '8080',
-      hmr: {
-        overlay: false,
-      },
-    },
-    plugins,
-    base: publicPath,
-    build: {
-      outDir,
-    },
-    resolve: {
-      alias: [
-        {
-          find: '@',
-          replacement: fileURLToPath(new URL('./src', import.meta.url)),
+    const plugins = await loadPlugins();
+
+    return {
+        server: {
+            host: '::',
+            port: '8080',
+            hmr: {
+                overlay: false,
+            },
         },
-        {
-          find: 'lib',
-          replacement: resolve(__dirname, 'lib'),
+        plugins,
+        base: '/',
+        build: {
+            outDir: 'build',
         },
-      ],
-    },
-  };
+        resolve: {
+            alias: [
+                {
+                    find: '@',
+                    replacement: fileURLToPath(new URL('./src', import.meta.url)),
+                },
+                {
+                    find: 'lib',
+                    replacement: resolve(__dirname, 'lib'),
+                },
+            ],
+        },
+    };
 });
